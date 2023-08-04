ed77fb2f63b826d8b037388a2db7e90794ff7ede
Vanshaj kaushal <66334366+vjkaal@users.noreply.github.com>
2023-08-04 09:47:38 +0530

    Initial commit

---
+++ university.cpp
@@ -0,0 +1,12 @@
+﻿// university.cpp : Defines the entry point for the application.
+//
+
+#include "inc/university.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Univesity Application" << endl;
+	return 0;
+}
